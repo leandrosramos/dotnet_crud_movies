9aec5e913fb22a7e3efa5edf562bc936724381d2
leandrosramos <leandro.sramos@outlook.com>
2021-02-21 20:16:18 -0300

    Criando o cadastro de Filmes.

---
+++ Classes/EntidadeBase.cs
@@ -0,0 +1,7 @@
+namespace dotnet_crud_movies
+{
+    public abstract class EntidadeBase
+    {
+        public int Id {get; protected set;}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace dotnet_crud_movies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // EntidadeBase minhaClasse = new EntidadeBase();
+            Serie meuObjeto = new Serie();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
